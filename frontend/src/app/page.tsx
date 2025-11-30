7d3c49b976fe71356c7bd40eb849f7d8d066878e
awesome-urch <nnabugwu.michael@gmail.com>
2025-11-30 19:06:05 +0100

    updated year

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -232,7 +232,7 @@ export default function LandingPage() {
             </div>
           </div>
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; 2024 SchoolFees. All rights reserved.</p>
+            <p>&copy; 2025 SchoolFees. All rights reserved.</p>
           </div>
         </div>
       </footer>
